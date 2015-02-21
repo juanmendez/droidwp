9ace23444143dc3230adb64f4682ff25430519bd
juanmendez <contact@juanmendez.info>
2015-02-21 12:43:43 -0600

    modifying demo using jackson jr

--- app/src/main/java/info/juanmendez/droidwp/MainActivity.java
+++ app/src/main/java/info/juanmendez/droidwp/MainActivity.java
@@ -1,7 +1,5 @@
 package info.juanmendez.droidwp;
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.ListView;
 
@@ -9,7 +7,6 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.InstanceState;
-import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.res.StringRes;
 
@@ -22,7 +19,7 @@ import info.juanmendez.droidwp.model.Band;
 import info.juanmendez.droidwp.service.JsonReader;
 
 @EActivity(R.layout.activity_main)
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements JsonReader.JsonReaderHandler {
 
     @ViewById
     ListView list;
@@ -39,50 +36,32 @@ public class MainActivity extends ActionBarActivity {
     public static final String tag = "info.juanmendez.droidwp";
 
     @InstanceState
-    public String bandsJson;
+    public String jsonSource;
 
     @AfterViews
     void afterViews() {
 
         try {
 
-            if( bandsJson != null )
+            if( jsonSource != null )
             {
-                jsonReader.readJson( bandsJson );
+                jsonReader.readJson(jsonSource, this );
             }
             else
             {
                 URI uri = new URI( ajax + "?action=droid_json_content&type=band-musician" );
-                jsonReader.readJson( uri );
+                jsonReader.readJson( uri, this );
             }
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
     }
 
-    @UiThread
-    public void loadContent( List<Band> bands )
-    {
-        bandsJson = jsonReader.getJsonString();
+    @Override
+    public void onContentReady(List<Band> bands, String jsonString ) {
+
+        jsonSource = jsonString;
         adapter.setList( bands );
         list.setAdapter( adapter );
     }
-
-    /**
-     * this method is for seeing the lifecycle, and is part of
-     * http://www.vogella.com/tutorials/AndroidLifeCycle/article.html
-     * @param methodName
-     */
-    private void notify(String methodName) {
-        String name = this.getClass().getName();
-        String[] strings = name.split("\\.");
-        Notification noti = new Notification.Builder(this)
-                .setContentTitle(methodName + " " + strings[strings.length - 1]).setAutoCancel(true)
-                .setSmallIcon(R.drawable.ic_launcher)
-                .setContentText(name).build();
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify((int) System.currentTimeMillis(), noti);
-    }
-
 }
